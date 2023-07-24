35165a7891255a3a9ba7abb82169c3f6d37af9be
Jaakko Husso <jaakko.t.husso@gmail.com>
2023-07-24 12:29:28 +0300

    Working on minification

--- demo-small.js
+++ demo-small.js
@@ -1,13 +1,98 @@
-(function () {
+function initializeSynth() {
+  const audioContext = new AudioContext();
+
+  // Lowpass filter
+  const lowpassFilterNode = audioContext.createBiquadFilter();
+  lowpassFilterNode.type = "lowpass";
+  lowpassFilterNode.frequency.value = (0.1 * audioContext.sampleRate) / 2;
+  lowpassFilterNode.Q.value = 10;
+
+  // Connections:
+  // OSC -> Lowpass -> Destination
+  lowpassFilterNode.connect(audioContext.destination);
+
+  const lfo = audioContext.createOscillator();
+  lfo.frequency.value = 5;
+  lfo.start();
+
+  const lfoGainNode = audioContext.createGain();
+  lfoGainNode.gain.value = 5;
+
+  lfo.connect(lfoGainNode);
+
+  // Oscilators: [octave, detune]
+  [
+    [3, 0],
+    [2, 20],
+    [2, -20],
+  ].map(([octave, detune]) => {
+    const oscillator = new OscillatorNode(audioContext);
+    oscillator.type = "sawtooth";
+    oscillator.frequency.value = 440 * Math.pow(2, octave - 4); // A
+    oscillator.detune.value = detune;
+
+    oscillator.connect(lowpassFilterNode);
+    lfoGainNode.connect(oscillator.frequency);
+
+    // A C F G
+    const f = [440, 523.25, 349.23, 392.0];
+    for (let i = 0; i < 100; i++) {
+      oscillator.frequency.setValueAtTime(
+        f[i % 4] * Math.pow(2, octave - 4),
+        audioContext.currentTime + i * 2
+      );
+    }
+
+    oscillator.start();
+
+    return oscillator;
+  });
+
+  let update = (angularVelocity1, angularVelocity2) => {
+    lowpassFilterNode.frequency.setValueAtTime(
+      (0.05 * Math.abs(angularVelocity2) * audioContext.sampleRate) / 2,
+      audioContext.currentTime
+    );
+    lowpassFilterNode.Q.setValueAtTime(
+      10 + angularVelocity1,
+      audioContext.currentTime
+    );
+    lfo.frequency.setValueAtTime(
+      Math.abs(angularVelocity2),
+      audioContext.currentTime
+    );
+    lfoGainNode.gain.setValueAtTime(
+      Math.abs(angularVelocity2),
+      audioContext.currentTime
+    );
+  };
+
+  return update;
+}
+
+function run() {
   const vs = `attribute vec2 p;void main(){gl_Position=vec4(p,0,1);}`;
-  const fs = `precision highp float;uniform float u_time;uniform vec2 u_resolution,u_test;const vec3 v=vec3(.8,1,1),f=vec3(.86,.83,.69),t=vec3(.14,.12,.58),z=vec3(1);float n(vec3 v,float z){return length(v)-z;}float e(vec3 v,float z){return v.y-z;}float e(vec3 v){float f=e(v,0.),z=n(v-vec3(0,1,0),.6);return min(f,z);}vec3 p(vec3 v,float f){vec3 y=exp2(-f*.01*z);return v*y+(1.-y)*t;}float e(vec3 v,vec3 f,float z){float r=1.,i=1.;for(int y=0;y<400;++y){if(i>=100.)return r;float n=e(f+i*v);if(n<1e-5)return 0.;r=min(r,z*n/i);i+=n;}return r;}vec3 n(vec3 v){float f=e(vec3(v.x+1e-5,v.yz))-e(vec3(v.x-1e-5,v.yz)),z=e(vec3(v.x,v.y+1e-5,v.z))-e(vec3(v.x,v.y-1e-5,v.z)),y=e(vec3(v.xy,v.z+1e-5))-e(vec3(v.xy,v.z-1e-5));return normalize(vec3(f,z,y));}mat4 n(vec3 v,vec3 f,vec3 z){vec3 y=normalize(f-v),r=normalize(cross(z,y)),i=cross(y,r);return mat4(vec4(r,0),vec4(i,0),vec4(-y,0),vec4(0,0,0,1));}vec3 e(vec3 z,vec3 y,vec3 r,vec3 i){vec3 m=n(y),u,x,t;float c=dot(m,z),g;u=vec3(0);if(c>0.)u=clamp(v*c*e(z,y,3.),0.,1.);x=clamp(v*(.5+.5*m.y)*(.5*f),0.,1.);g=dot(m,-z);t=vec3(0);if(g>0.)t=clamp(v*g*(.5*v),0.,1.);return clamp(i*(u+x+t),0.,1.);}float n(vec3 v,vec3 z,float n,float y){float f=(100.-v.y)/z.y,r,i,t;if(f>0.)y=min(y,f);r=0.;i=0.;t=n;for(int u=0;u<400;u++){r=.001*t;vec3 m=v+t*z;float c=e(m);if(c<r)break;t+=c*.5;if(t>=y)break;}if(t>=y)return-1.;return t;}vec3 p(float v,vec2 y,vec2 z){vec2 f=z-y/2.;float r=y.y/tan(radians(v)/2.);return normalize(vec3(f,-r));}void main(){vec3 v=p(60.,u_resolution,gl_FragCoord.xy),f=vec3(2,2,3),y=vec3(0,1,0),r,z;mat4 i=n(f,y,vec3(0,1,0));r=(i*vec4(v,0)).xyz;float t=n(f,r,0.,100.);z=normalize(vec3(1,10,1));if(t<0.)gl_FragColor=vec4(1,0,1,1);else{vec3 u=vec3(.6,.5,.4),x=f+t*r,c=e(z,x,f,u);c=p(c,t);c=pow(c,vec3(1,.92,1));c*=vec3(1.02,.99,.9);c.z=c.z+.1;c=smoothstep(0.,1.,c);gl_FragColor=vec4(c,1);}}`;
+  const fs = `precision highp float;uniform float v;uniform vec2 d;uniform vec4 m;uniform vec2 f;const vec3 i=vec3(.8,.7,.6),h=vec3(1,.92,1);struct M{vec3 d;vec3 m;vec3 e;float h;float f;};struct S{int i;float d;M m;};struct R{S d;vec3 n;vec3 o;bool h;};float n(vec3 v,vec3 d,vec3 e){vec3 i=v-d,h=e-d;return length(i-h*clamp(dot(i,h)/dot(h,h),0.,1.))-.05;}float n(vec3 v,float d){return length(v)-d;}float n(vec3 v){vec3 d=vec3(0,9,0),h=vec3(m.xy,0)+d,i=vec3(m.zw,0)+d;float e=n(v-h,f.x),y=n(v-i,f.y),c=n(v,d,h),o=n(v,h,i);return min(min(e,y),min(c,o));}S e(S d, S h){if (d.d < h.d) return d; else return h;}S e(vec3 v){S i,h=S(0,n(v),M(vec3(.5),vec3(.5),vec3(.5),50.,.5)),d=S(1,dot(v,vec3(0,1,0))+2.,M(vec3(.5,1,1),vec3(.5),vec3(.5),2.,0.)),y=S(2,n(v-vec3(5),5.),M(vec3(.9),vec3(.5),vec3(.5),50.,.5)),o=S(3,n(v-vec3(-2,2,-3),3.),M(vec3(1,.3,.2),vec3(.9,.5,.5),vec3(.9),50.,.3));i=e(d,h);i=e(i,y);return e(i,o);}vec3 t(vec3 d,float v){vec3 m=exp2(-v*.05*h);return d*m+(1.-m)*i;}vec3 e(vec3 d,vec3 v,vec3 m){vec3 e=vec3(.7,.8,.9)-.5*v.y,y=exp2(-abs((2.5e4-d.y)/v.y)*1e-5*h);e=e*y+(1.-y)*i;float o=dot(m,v);if(o>.9999)e=vec3(.9);if(o>.9998)e=mix(e,vec3(.9),(o-.9998)/2e-4);return e;}float s(vec3 v,vec3 d){float i=1.,h=1.;for(int f=0;f<400;++f){if(h>=250.)return i;S m=e(d+h*v);if(m.d<1e-5)return 0.;i=min(i,10.*m.d/h);h+=m.d;}return i;}mat4 p(vec3 v,vec3 d){vec3 h=normalize(vec3(0,5,0)-v),i=normalize(cross(d,h));return mat4(vec4(i,0),vec4(cross(h,i),0),vec4(-h,0),vec4(0,0,0,1));}vec3 e(vec3 v,vec3 h,vec3 d,vec3 i,M m){float e=clamp(dot(v,h)*s(v,d),0.,1.);return m.m+m.d*e+m.e*pow(clamp(dot(reflect(v,h),i),0.,1.),m.h);}R r(vec3 v,vec3 d){float h=1e-5,i=1e-5;R m;for(int f=0;f<400;f++){h=.001*i;m.o=v+i*d;m.d=e(m.o);if(m.d.d<h){m.h=true;break;}i+=m.d.d*.5;if(i>=250.)break;}m.d.d=i;const vec2 f=vec2(1,-1);vec3 o=m.o;m.n=normalize(f.xyy*e(o+f.xyy*1e-5).d+f.yyx*e(o+f.yyx*1e-5).d+f.yxy*e(o+f.yxy*1e-5).d+f.xxx*e(o+f.xxx*1e-5).d);return m;}vec3 x(vec3 v,vec3 d){vec3 h=normalize(vec3(0,10,10)),i=vec3(0);float f=1.;vec3 m=d;float o=0.;R n=r(v,m);for(int s=0;s<10;s++){if(!n.h){i=mix(i,e(v,m,h),f);break;}vec3 y=n.n;if(n.d.i==1)y=vec3(0,1,0);else if(n.d.i==2)y=normalize(n.o-vec3(5));else if(n.d.i==3)y=normalize(n.o-vec3(-1,2,-3));o+=n.d.d;vec3 c=e(h,y,n.o,m,n.d.m);c=t(c,o);i=mix(i,c,f);f*=n.d.m.f;if(f<1e-5)break;m=reflect(m,y);n=r(n.o,m);}return i;}void main(){vec2 i=gl_FragCoord.xy-d/2.;vec3 m=vec3(-10.+20.*sin(v/1e4),4.+sin(v/5e3),-20.+20.*cos(v/1e4));mat4 e=p(m,normalize(vec3(1.-sin(v/5e3),sin(v/5e3),0)));vec3 f=x(m,(e*vec4(normalize(vec3(i,-d.y/tan(radians(60.)/2.))),0)).xyz);f=pow(f,h);f*=vec3(1.02,.99,.9);f.z=f.z+.1;f=smoothstep(0.,1.,f);gl_FragColor=vec4(f,1);}`;
 
   const programs = [];
 
-  let gl, canvas;
+  let gl, canvas, updateNote, now, dt;
+  let lastRenderTime = performance.now();
 
-  // Constants, shorter than using full gl.NAME variants.
-  var GL_VERTEX_SHADER = 0x8b31,
+  let pendulum = {
+    angle1: Math.PI * 1.0,
+    angle2: Math.PI * 0.5,
+    angularVelocity1: 1.0,
+    angularVelocity2: 0.0,
+    length1: 5.0,
+    length2: 2.0,
+    mass1: 1.0,
+    mass2: 5.0,
+    g: 9.81,
+    damping: 0.0,
+  };
+
+  let GL_VERTEX_SHADER = 0x8b31,
     GL_FRAGMENT_SHADER = 0x8b30,
     GL_ARRAY_BUFFER = 0x8892,
     GL_STATIC_DRAW = 0x88e4,
@@ -18,7 +103,93 @@
   const width = window.innerWidth,
     height = window.innerHeight;
 
-  function loop() {
+  // Based on math here:
+  // https://diego.assencio.com/?index=1500c66ae7ab27bb0106467c68feebc6
+  function lagrange(angle1, angle2, angularVelocity1, angularVelocity2) {
+    const { length1, length2, mass1, mass2, g, damping } = pendulum;
+
+    let a1 =
+      (length2 / length1) *
+      (mass2 / (mass1 + mass2)) *
+      Math.cos(angle1 - angle2);
+    let a2 = (length1 / length2) * Math.cos(angle1 - angle2);
+
+    let f1 =
+      -(length2 / length1) *
+        (mass2 / (mass1 + mass2)) *
+        Math.pow(angularVelocity2, 2) *
+        Math.sin(angle1 - angle2) -
+      (g / length1) * Math.sin(angle1);
+    let f2 =
+      (length1 / length2) *
+        Math.pow(angularVelocity1, 2) *
+        Math.sin(angle1 - angle2) -
+      (g / length2) * Math.sin(angle2);
+
+    let g1 = (f1 - a1 * f2) / (1.0 - a1 * a2) - damping * angularVelocity1;
+    let g2 = (f2 - a2 * f1) / (1.0 - a1 * a2) - damping * angularVelocity2;
+
+    return [angularVelocity1, angularVelocity2, g1, g2];
+  }
+
+  function update(pendulum, dt) {
+    const { angle1, angle2, angularVelocity1, angularVelocity2 } = pendulum;
+
+    // Runga-Kutta method
+    let k1 = lagrange(angle1, angle2, angularVelocity1, angularVelocity2);
+    let k2 = lagrange(
+      angle1 + (dt * k1[0]) / 2.0,
+      angle2 + (dt * k1[1]) / 2.0,
+      angularVelocity1 + (dt * k1[2]) / 2.0,
+      angularVelocity2 + (dt * k1[3]) / 2.0
+    );
+    let k3 = lagrange(
+      angle1 + (dt * k2[0]) / 2.0,
+      angle2 + (dt * k2[1]) / 2.0,
+      angularVelocity1 + (dt * k2[2]) / 2.0,
+      angularVelocity2 + (dt * k2[3]) / 2.0
+    );
+    let k4 = lagrange(
+      angle1 + dt * k3[0],
+      angle2 + dt * k3[1],
+      angularVelocity1 + dt * k3[2],
+      angularVelocity2 + dt * k3[3]
+    );
+
+    pendulum.angle1 += (dt * (k1[0] + 2.0 * k2[0] + 2.0 * k3[0] + k4[0])) / 6.0;
+    pendulum.angle2 += (dt * (k1[1] + 2.0 * k2[1] + 2.0 * k3[1] + k4[1])) / 6.0;
+    pendulum.angularVelocity1 +=
+      (dt * (k1[2] + 2.0 * k2[2] + 2.0 * k3[2] + k4[2])) / 6.0;
+    pendulum.angularVelocity2 +=
+      (dt * (k1[3] + 2.0 * k2[3] + 2.0 * k3[3] + k4[3])) / 6.0;
+  }
+
+  function position(pendulum) {
+    return {
+      pos1: {
+        x: Math.sin(pendulum.angle1) * pendulum.length1,
+        y: -Math.cos(pendulum.angle1) * pendulum.length1,
+      },
+      pos2: {
+        x:
+          Math.sin(pendulum.angle1) * pendulum.length1 +
+          Math.sin(pendulum.angle2) * pendulum.length2,
+        y:
+          -Math.cos(pendulum.angle1) * pendulum.length1 -
+          Math.cos(pendulum.angle2) * pendulum.length2,
+      },
+    };
+  }
+
+  function render() {
+    now = performance.now();
+    dt = (now - lastRenderTime) / 1000;
+    lastRenderTime = now;
+
+    update(pendulum, dt);
+    const { pos1, pos2 } = position(pendulum);
+    updateNote(pendulum.angularVelocity1, pendulum.angularVelocity2);
+
     gl.clearColor(0, 0, 0, 1);
     gl.clear(GL_COLOR_BUFFER_BIT);
 
@@ -27,12 +198,24 @@
 
     const time = performance.now();
 
-    gl.uniform1f(gl.getUniformLocation(program, "u_time"), time);
-    gl.uniform2f(gl.getUniformLocation(program, "u_resolution"), width, height);
+    gl.uniform1f(gl.getUniformLocation(program, "v"), now);
+    gl.uniform2f(gl.getUniformLocation(program, "d"), width, height);
+    gl.uniform4f(
+      gl.getUniformLocation(program, "m"),
+      pos1.x,
+      pos1.y,
+      pos2.x,
+      pos2.y
+    );
+    gl.uniform2f(
+      gl.getUniformLocation(program, "f"),
+      0.2 * Math.pow(pendulum.mass1, 0.333),
+      0.2 * Math.pow(pendulum.mass2, 0.333)
+    );
 
     gl.drawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-    window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(render);
   }
 
   (function setup() {
@@ -45,7 +228,7 @@
     s.position = "fixed";
     s.left = s.top = 0;
 
-    gl = canvas.getContext("experimental-webgl");
+    gl = canvas.getContext("webgl");
 
     gl.viewport(0, 0, width, height);
 
@@ -72,6 +255,8 @@
     gl.enableVertexAttribArray(0);
     gl.vertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
 
-    window.requestAnimationFrame(loop);
+    updateNote = initializeSynth();
+
+    window.requestAnimationFrame(render);
   })();
-})();
+}
--- fragment.glsl
+++ fragment.glsl
@@ -1,110 +1,53 @@
 precision highp float;
 uniform float u_time;
 uniform vec2 u_resolution;
-uniform vec2 u_test;
-uniform sampler2D u_audio;
+uniform vec4 u_p;
+uniform vec2 u_r;
 
-uniform vec2 u_p1;
-uniform vec2 u_p2;
-uniform float u_r1;
-uniform float u_r2;
-
-const int MAX_MARCHING_STEPS = 400;
-const int REFLECTIONS = 10;
-const float MIN_DIST = 0.0;
 const float MAX_DIST = 250.0;
-const float FOV = 60.0;
-const float EPSILON = 0.00001;
-const float SPEED = 500.0;
-const float PI = 3.14159;
+const float EPSILON = .00001;
 
-const vec3 SUN_COLOR = vec3(0.9, 0.9, 0.9);
-const vec3 SKY_COLOR = vec3(0.7, 0.8, 0.9);
-const vec3 FOG_COLOR = vec3(0.8, 0.7, 0.6);
-const vec3 COLOR_SHIFT = vec3(1.0, 0.92, 1.0);
+const vec3 FOG_COLOR = vec3(.8, .7, .6);
+const vec3 COLOR_SHIFT = vec3(1., .92, 1.);
 
-struct Material {
-  vec3 diffuse;
-  vec3 ambient;
-  vec3 specular;
-  float shininess;
-  float reflection;
+struct M {
+  vec3 d;
+  vec3 m;
+  vec3 e;
+  float h;
+  float f;
 };
 
-struct Surface {
-  int id;
-  float dist;
-  Material material;
+struct S {
+  int i;
+  float d;
+  M m;
 };
 
-// Translations
-// http://en.wikipedia.org/wiki/Rotation_matrix#Basic_rotations
-mat3 tRotateX(float theta) {
-  float s = sin(theta);
-  float c = cos(theta);
-
-  return mat3(vec3(1, 0, 0), vec3(0, c, -s), vec3(0, s, c));
-}
-
-mat3 tRotateY(float theta) {
-  float s = sin(theta);
-  float c = cos(theta);
-
-  return mat3(vec3(c, 0, s), vec3(0, 1, 0), vec3(-s, 0, c));
-}
-
-mat3 tRotateZ(float theta) {
-  float s = sin(theta);
-  float c = cos(theta);
-
-  return mat3(vec3(c, -s, 0), vec3(s, c, 0), vec3(0, 0, 1));
-}
-
-// From https://www.shadertoy.com/view/ldl3W8
-vec2 hash2(vec2 p) {
-  return fract(
-      sin(vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)))) *
-      43758.5453);
-}
-
-// https://www.shadertoy.com/view/lsl3RH
-float noise(in vec2 p) { return sin(p.x) * sin(p.y); }
-
-const mat2 m = mat2(0.80, 0.60, -0.60, 0.80);
-
-// https://www.shadertoy.com/view/lsl3RH
-float fbm4(vec2 p) {
-  float f = 0.0;
-  f += 0.5000 * noise(p);
-  p = m * p * 2.02;
-  f += 0.2500 * noise(p);
-  p = m * p * 2.03;
-  f += 0.1250 * noise(p);
-  p = m * p * 2.01;
-  f += 0.0625 * noise(p);
-  return f / 0.9375;
-}
+struct R {
+  S d;
+  vec3 n;
+  vec3 o;
+  bool h;
+};
 
 // https://iquilezles.org/articles/distfunctions/, MIT
 float sdCapsule(vec3 p, vec3 a, vec3 b, float r) {
   vec3 pa = p - a, ba = b - a;
-  float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+  float h = clamp(dot(pa, ba) / dot(ba, ba), .0, 1.);
   return length(pa - ba * h) - r;
 }
-
-// https://iquilezles.org/articles/distfunctions/, MIT
-float sdPlane(vec3 p, vec3 n, float h) { return dot(p, n) + h; }
 
 // https://iquilezles.org/articles/distfunctions/, MIT
 float sdSphere(vec3 p, float s) { return length(p) - s; }
 
 float sdPendulum(in vec3 p) {
-  vec3 offset = vec3(0.0, 9., 0.0);
-  vec3 p1 = vec3(u_p1.x, u_p1.y, 0.0) + offset;
-  vec3 p2 = vec3(u_p2.x, u_p2.y, 0.0) + offset;
+  vec3 offset = vec3(0., 9., 0.);
+  vec3 p1 = vec3(u_p.xy, 0.) + offset;
+  vec3 p2 = vec3(u_p.zw, 0.) + offset;
 
-  float ball1 = sdSphere(p - p1, u_r1);
-  float ball2 = sdSphere(p - p2, u_r2);
+  float ball1 = sdSphere(p - p1, u_r.x);
+  float ball2 = sdSphere(p - p2, u_r.y);
 
   float line1 = sdCapsule(p, offset, p1, 0.05);
   float line2 = sdCapsule(p, p1, p2, 0.05);
@@ -112,30 +55,25 @@ float sdPendulum(in vec3 p) {
   return min(min(ball1, ball2), min(line1, line2));
 }
 
-// https://iquilezles.org/articles/distfunctions/, MIT
-float sdCylinder(vec3 p, vec3 c) { return length(p.xz - c.xy) - c.z; }
-
-Surface opUnion(Surface a, Surface b) {
-  if (a.dist < b.dist) {
+S opUnion(S a, S b) {
+  if (a.d < b.d) {
     return a;
   }
   return b;
 }
 
-Surface scene(in vec3 p) {
-  Surface surface;
+S scene(in vec3 p) {
+  S surface;
 
-  Surface pendulum = Surface(
-      0, sdPendulum(p), Material(vec3(0.5), vec3(0.5), vec3(0.5), 50.0, 0.5));
-  Surface ground =
-      Surface(1, sdPlane(p, vec3(0., 1., 0.), 2.0),
-              Material(vec3(0.5, 1.0, 1.0), vec3(0.5), vec3(0.5), 2.0, 0.0));
-  Surface sphere1 =
-      Surface(2, sdSphere(p - vec3(5.0), 5.0),
-              Material(vec3(0.9), vec3(0.5), vec3(0.5), 50.0, 0.5));
-  Surface sphere2 = Surface(
-      3, sdSphere(p - vec3(-2.0, 2.0, -3.0), 3.0),
-      Material(vec3(1.0, 0.3, 0.2), vec3(0.9, 0.5, 0.5), vec3(0.9), 50.0, 0.3));
+  S pendulum =
+      S(0, sdPendulum(p), M(vec3(0.5), vec3(0.5), vec3(0.5), 50.0, 0.5));
+  S ground = S(1, dot(p, vec3(0., 1., 0.)) + 2.0,
+               M(vec3(0.5, 1.0, 1.0), vec3(0.5), vec3(0.5), 2.0, 0.0));
+  S sphere1 = S(2, sdSphere(p - vec3(5.0), 5.0),
+                M(vec3(0.9), vec3(0.5), vec3(0.5), 50.0, 0.5));
+  S sphere2 =
+      S(3, sdSphere(p - vec3(-2.0, 2.0, -3.0), 3.0),
+        M(vec3(1.0, 0.3, 0.2), vec3(0.9, 0.5, 0.5), vec3(0.9), 50.0, 0.3));
 
   surface = opUnion(ground, pendulum);
   surface = opUnion(surface, sphere1);
@@ -145,61 +83,52 @@ Surface scene(in vec3 p) {
 }
 
 vec3 fog(in vec3 color, float dist) {
-  vec3 e = exp2(-dist * 0.055 * COLOR_SHIFT);
-  return color * e + (1.0 - e) * FOG_COLOR;
+  vec3 e = exp2(-dist * .05 * COLOR_SHIFT);
+  return color * e + (1. - e) * FOG_COLOR;
 }
 
 vec3 sky(in vec3 camera, in vec3 dir, in vec3 sun) {
   // Deeper blue when looking up
-  vec3 color = SKY_COLOR - 0.5 * dir.y;
+  vec3 color = vec3(.7, .8, .9) - .5 * dir.y;
 
   // Fade to fog further away
   float dist = (25000. - camera.y) / dir.y;
-  vec3 e = exp2(-abs(dist) * 0.00001 * COLOR_SHIFT);
+  vec3 e = exp2(-abs(dist) * EPSILON * COLOR_SHIFT);
   color = color * e + (1.0 - e) * FOG_COLOR;
 
   // Sun
   float dotSun = dot(sun, dir);
-  if (dotSun > 0.9999) {
+  if (dotSun > .9999) {
     float h = dir.y - sun.y;
-    color = SUN_COLOR;
+    color = vec3(.9);
   }
 
   // Sun glare
-  if (dotSun > 0.9998) {
-    color = mix(color, SUN_COLOR, (dotSun - 0.9998) / (1. - 0.9998));
+  if (dotSun > .9998) {
+    color = mix(color, vec3(.9), (dotSun - .9998) / (1. - .9998));
   }
 
   return color;
 }
 
 float softShadows(in vec3 sunDir, in vec3 p, float k) {
-  float opacity = 1.0;
-  float depth = 1.0;
+  float opacity = 1.;
+  float depth = 1.;
 
-  for (int s = 0; s < MAX_MARCHING_STEPS; ++s) {
+  for (int s = 0; s < 400; ++s) {
     if (depth >= MAX_DIST) {
       return opacity;
     }
 
-    Surface surface = scene(p + depth * sunDir);
-    if (surface.dist < EPSILON) {
-      return 0.0;
+    S surface = scene(p + depth * sunDir);
+    if (surface.d < EPSILON) {
+      return 0.;
     }
-    opacity = min(opacity, k * surface.dist / depth);
-    depth += surface.dist;
+    opacity = min(opacity, k * surface.d / depth);
+    depth += surface.d;
   }
 
   return opacity;
-}
-
-// Tetrahedron technique, https://iquilezles.org/articles/normalsSDF/
-vec3 calcNormal(in vec3 p) {
-  const vec2 k = vec2(1, -1);
-  return normalize(k.xyy * scene(p + k.xyy * EPSILON).dist +
-                   k.yyx * scene(p + k.yyx * EPSILON).dist +
-                   k.yxy * scene(p + k.yxy * EPSILON).dist +
-                   k.xxx * scene(p + k.xxx * EPSILON).dist);
 }
 
 mat4 lookAt(vec3 camera, vec3 target, vec3 up) {
@@ -207,93 +136,60 @@ mat4 lookAt(vec3 camera, vec3 target, vec3 up) {
   vec3 s = normalize(cross(up, f));
   vec3 u = cross(f, s);
 
-  return mat4(vec4(s, 0.0), vec4(u, 0.0), vec4(-f, 0.0),
-              vec4(0.0, 0.0, 0.0, 1.0));
+  return mat4(vec4(s, .0), vec4(u, .0), vec4(-f, .0), vec4(.0, .0, .0, 1.));
 }
 
-vec3 lightning(in vec3 sun, in vec3 p, in vec3 normal, in vec3 material) {
-  float beat = texture2D(u_audio, vec2(0.0, 0.5)).r * 3.;
-
-  float dotNS = dot(normal, sun);
-  vec3 sunLight = vec3(0.0);
-  if (dotNS > 0.0) {
-    sunLight = clamp(SUN_COLOR * dotNS * softShadows(sun, p, 10.0), 0.0, 1.0);
-  }
-
-  vec3 skyLight =
-      clamp(SUN_COLOR * (0.5 + 0.5 * normal.y) * (0.5 * SKY_COLOR), 0.0, 1.0);
-
-  float dotNB = dot(normal, -sun);
-  vec3 bounceLight = vec3(0.0);
-  if (dotNB > 0.0) {
-    bounceLight = clamp(SUN_COLOR * dotNB * (0.4 * SUN_COLOR), 0.0, 1.0);
-  }
-
-  return clamp(material * (skyLight + sunLight + bounceLight), 0.0, 1.0);
-}
-
-vec3 phong(in vec3 sun, in vec3 normal, in vec3 p, in vec3 rayDir,
-           Material material) {
-  vec3 ambient = material.ambient;
+vec3 phong(in vec3 sun, in vec3 normal, in vec3 p, in vec3 rayDir, M material) {
+  vec3 ambient = material.m;
 
   float dotLN = clamp(dot(sun, normal) * softShadows(sun, p, 10.0), 0., 1.);
-  vec3 diffuse = material.diffuse * dotLN;
-
-  // vec3 h = normalize(sun + rayDir);
-  // float dotNH = clamp(dot(normal, h), 0., 1.);
-  // vec3 specular = material.specular * pow(dotNH, material.shininess);
+  vec3 diffuse = material.d * dotLN;
 
   float dotRV = clamp(dot(reflect(sun, normal), rayDir), 0., 1.);
-  vec3 specular = material.specular * pow(dotRV, material.shininess);
+  vec3 specular = material.e * pow(dotRV, material.h);
 
   return ambient + diffuse + specular;
 }
 
-struct RayResult {
-  Surface surface;
-  vec3 normal;
-  vec3 pos;
-  bool hit;
-};
+R rayMarch(in vec3 camera, in vec3 rayDir) {
+  float stepDist = EPSILON;
+  float dist = EPSILON;
+  float depth = EPSILON;
 
-RayResult rayMarch(in vec3 camera, in vec3 rayDir, float start, float end) {
-  // Check if we would hit the bounding plane before reaching "end"
-  float stepsToBoundingPlane = (100.0 - camera.y) / rayDir.y;
-  if (stepsToBoundingPlane > 0.0) {
-    end = min(end, stepsToBoundingPlane);
-  }
+  R result;
 
-  float stepDist = 0.0;
-  float dist = 0.0;
-  float depth = start;
-
-  RayResult result;
-
-  for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+  for (int i = 0; i < 400; i++) {
     stepDist = 0.001 * depth;
 
-    result.pos = camera + depth * rayDir;
-    result.surface = scene(result.pos);
+    result.o = camera + depth * rayDir;
+    result.d = scene(result.o);
 
-    if (result.surface.dist < stepDist) {
-      result.hit = true;
+    if (result.d.d < stepDist) {
+      result.h = true;
       break;
     }
 
-    depth += result.surface.dist * 0.5;
+    depth += result.d.d * 0.5;
 
-    if (depth >= end) {
+    if (depth >= MAX_DIST) {
       break;
     }
   }
 
-  result.surface.dist = depth;
-  result.normal = calcNormal(result.pos);
+  result.d.d = depth;
+
+  // Tetrahedron technique, https://iquilezles.org/articles/normalsSDF/
+  const vec2 k = vec2(1, -1);
+  vec3 p = result.o;
+  result.n = normalize(k.xyy * scene(p + k.xyy * EPSILON).d +
+                       k.yyx * scene(p + k.yyx * EPSILON).d +
+                       k.yxy * scene(p + k.yxy * EPSILON).d +
+                       k.xxx * scene(p + k.xxx * EPSILON).d);
 
   return result;
 }
 
-vec3 render(in vec3 camera, in vec3 rayDir, float start, float end) {
+vec3 render(in vec3 camera, in vec3 rayDir) {
   vec3 sun = normalize(vec3(0.0, 10.0, 10.0));
 
   vec3 color = vec3(0.0);
@@ -301,67 +197,65 @@ vec3 render(in vec3 camera, in vec3 rayDir, float start, float end) {
   vec3 dir = rayDir;
 
   float rayDist = 0.0;
-  RayResult ray = rayMarch(camera, dir, start, end);
+  R ray = rayMarch(camera, dir);
 
-  for (int i = 0; i < REFLECTIONS; i++) {
-    if (!ray.hit) {
+  for (int i = 0; i < 10; i++) {
+    if (!ray.h) {
       color = mix(color, sky(camera, dir, sun), reflection);
       break;
     }
 
-    vec3 normal = ray.normal;
+    vec3 normal = ray.n;
 
     // More accurate normals
-    if (ray.surface.id == 1) {
+    if (ray.d.i == 1) {
       normal = vec3(0.0, 1.0, 0.0);
-    } else if (ray.surface.id == 2) {
-      normal = normalize(ray.pos - vec3(5.0));
-    } else if (ray.surface.id == 3) {
-      normal = normalize(ray.pos - vec3(-1.0, 2.0, -3.0));
+    } else if (ray.d.i == 2) {
+      normal = normalize(ray.o - vec3(5.0));
+    } else if (ray.d.i == 3) {
+      normal = normalize(ray.o - vec3(-1.0, 2.0, -3.0));
     }
 
-    rayDist += ray.surface.dist;
-    vec3 newColor = phong(sun, normal, ray.pos, dir, ray.surface.material);
+    rayDist += ray.d.d;
+    vec3 newColor = phong(sun, normal, ray.o, dir, ray.d.m);
     newColor = fog(newColor, rayDist);
 
     color = mix(color, newColor, reflection);
 
-    reflection *= ray.surface.material.reflection;
+    reflection *= ray.d.m.f;
     if (reflection < EPSILON) {
       break;
     }
 
     dir = reflect(dir, normal);
-    ray = rayMarch(ray.pos, dir, EPSILON, MAX_DIST);
+    ray = rayMarch(ray.o, dir);
   }
 
   return color;
 }
 
-vec3 rayDirection(float fov, vec2 dimensions, vec2 fragCoord) {
-  vec2 xy = fragCoord - dimensions / 2.0;
-  float z = dimensions.y / tan(radians(fov) / 2.0);
-  return normalize(vec3(xy, -z));
-}
-
 void main() {
-  vec3 viewDir = rayDirection(FOV, u_resolution, gl_FragCoord.xy);
+  vec2 xy = gl_FragCoord.xy - u_resolution / 2.0;
+  float z = u_resolution.y / tan(radians(60.0) / 2.0);
+  vec3 viewDir = normalize(vec3(xy, -z));
 
-  vec3 camera = vec3(-10. + 20. * sin(u_time / (20. * SPEED)),
-                     4. + sin(u_time / (10. * SPEED)),
-                     -20. + 20. * cos(u_time / (20. * SPEED)));
+  float speed = 500.;
 
-  // vec3 camera = vec3(-20, 2, -20);
+  vec3 camera = vec3(-10. + 20. * sin(u_time / (20. * speed)),
+                     4. + sin(u_time / (10. * speed)),
+                     -20. + 20. * cos(u_time / (20. * speed)));
+
   vec3 target = vec3(0, 5, 0);
 
-  // mat4 viewToWorld = lookAt(camera, target, normalize(vec3(1. - sin(u_time
-  // / (10. * SPEED)), sin(u_time / (10. * SPEED)), 0.0)));
-  mat4 viewToWorld = lookAt(camera, target, normalize(vec3(0., 1., 0.)));
+  mat4 viewToWorld = lookAt(camera, target,
+                            normalize(vec3(1. - sin(u_time / (10. * speed)),
+                                           sin(u_time / (10. * speed)), 0.0)));
+  // mat4 viewToWorld = lookAt(camera, target, normalize(vec3(0., 1., 0.)));
   vec3 worldDir = (viewToWorld * vec4(viewDir, 0.0)).xyz;
 
-  vec3 color = render(camera, worldDir, MIN_DIST, MAX_DIST);
+  vec3 color = render(camera, worldDir);
 
-  color = pow(color, vec3(1.0, 0.92, 1.0));
+  color = pow(color, COLOR_SHIFT);
   color *= vec3(1.02, 0.99, 0.9);
   color.z = color.z + 0.1;
 
