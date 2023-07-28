9fb93625911876800390771f1e3baf72db5bac93
Jaakko Husso <jaakko.t.husso@gmail.com>
2023-07-28 19:53:04 +0300

    Framebuffers to average two frames

--- demo.js
+++ demo.js
@@ -376,16 +376,18 @@ async function run() {
   const fs = await res.text();
   // const fs = `precision highp float;uniform float v;uniform vec2 d;uniform vec4 m;uniform vec2 f;const vec3 i=vec3(.8,.7,.6),h=vec3(1,.92,1);struct M{vec3 d;vec3 m;vec3 e;float h;float f;};struct S{int i;float d;M m;};struct R{S d;vec3 n;vec3 o;bool h;};float n(vec3 v,vec3 d,vec3 e){vec3 i=v-d,h=e-d;return length(i-h*clamp(dot(i,h)/dot(h,h),0.,1.))-.05;}float n(vec3 v,float d){return length(v)-d;}float n(vec3 v){vec3 d=vec3(0,9,0),h=vec3(m.xy,0)+d,i=vec3(m.zw,0)+d;float e=n(v-h,f.x),y=n(v-i,f.y),c=n(v,d,h),o=n(v,h,i);return min(min(e,y),min(c,o));}S e(S d, S h){if (d.d < h.d) return d; else return h;}S e(vec3 v){S i,h=S(0,n(v),M(vec3(.5),vec3(.5),vec3(.5),50.,.5)),d=S(1,dot(v,vec3(0,1,0))+2.,M(vec3(.5,1,1),vec3(.5),vec3(.5),2.,0.)),y=S(2,n(v-vec3(5),5.),M(vec3(.9),vec3(.5),vec3(.5),50.,.5)),o=S(3,n(v-vec3(-2,2,-3),3.),M(vec3(1,.3,.2),vec3(.9,.5,.5),vec3(.9),50.,.3));i=e(d,h);i=e(i,y);return e(i,o);}vec3 t(vec3 d,float v){vec3 m=exp2(-v*.05*h);return d*m+(1.-m)*i;}vec3 e(vec3 d,vec3 v,vec3 m){vec3 e=vec3(.7,.8,.9)-.5*v.y,y=exp2(-abs((2.5e4-d.y)/v.y)*1e-5*h);e=e*y+(1.-y)*i;float o=dot(m,v);if(o>.9999)e=vec3(.9);if(o>.9998)e=mix(e,vec3(.9),(o-.9998)/2e-4);return e;}float s(vec3 v,vec3 d){float i=1.,h=1.;for(int f=0;f<400;++f){if(h>=250.)return i;S m=e(d+h*v);if(m.d<1e-5)return 0.;i=min(i,10.*m.d/h);h+=m.d;}return i;}mat4 p(vec3 v,vec3 d){vec3 h=normalize(vec3(0,5,0)-v),i=normalize(cross(d,h));return mat4(vec4(i,0),vec4(cross(h,i),0),vec4(-h,0),vec4(0,0,0,1));}vec3 e(vec3 v,vec3 h,vec3 d,vec3 i,M m){float e=clamp(dot(v,h)*s(v,d),0.,1.);return m.m+m.d*e+m.e*pow(clamp(dot(reflect(v,h),i),0.,1.),m.h);}R r(vec3 v,vec3 d){float h=1e-5,i=1e-5;R m;for(int f=0;f<400;f++){h=.001*i;m.o=v+i*d;m.d=e(m.o);if(m.d.d<h){m.h=true;break;}i+=m.d.d*.5;if(i>=250.)break;}m.d.d=i;const vec2 f=vec2(1,-1);vec3 o=m.o;m.n=normalize(f.xyy*e(o+f.xyy*1e-5).d+f.yyx*e(o+f.yyx*1e-5).d+f.yxy*e(o+f.yxy*1e-5).d+f.xxx*e(o+f.xxx*1e-5).d);return m;}vec3 x(vec3 v,vec3 d){vec3 h=normalize(vec3(0,10,10)),i=vec3(0);float f=1.;vec3 m=d;float o=0.;R n=r(v,m);for(int s=0;s<10;s++){if(!n.h){i=mix(i,e(v,m,h),f);break;}vec3 y=n.n;if(n.d.i==1)y=vec3(0,1,0);else if(n.d.i==2)y=normalize(n.o-vec3(5));else if(n.d.i==3)y=normalize(n.o-vec3(-1,2,-3));o+=n.d.d;vec3 c=e(h,y,n.o,m,n.d.m);c=t(c,o);i=mix(i,c,f);f*=n.d.m.f;if(f<1e-5)break;m=reflect(m,y);n=r(n.o,m);}return i;}void main(){vec2 i=gl_FragCoord.xy-d/2.;vec3 m=vec3(-10.+20.*sin(v/1e4),4.+sin(v/5e3),-20.+20.*cos(v/1e4));mat4 e=p(m,normalize(vec3(1.-sin(v/5e3),sin(v/5e3),0)));vec3 f=x(m,(e*vec4(normalize(vec3(i,-d.y/tan(radians(60.)/2.))),0)).xyz);f=pow(f,h);f*=vec3(1.02,.99,.9);f.z=f.z+.1;f=smoothstep(0.,1.,f);gl_FragColor=vec4(f,1);}`;
 
+  const res2 = await fetch("/effect.glsl");
+  const fs2 = await res2.text();
+
   const programs = [];
+  const textures = [];
+  const framebuffers = [];
 
   let gui;
   let synth;
+  let frame = 0;
 
   let gl, mCanvas, mWidth, mHeight;
-
-  let fft;
-  let analyser;
-  let texture;
   let lastRenderTime = performance.now();
 
   let pendulum = {
@@ -503,10 +505,15 @@ async function run() {
       if (width != mWidth || height != mHeight) {
         mCanvas.width = width;
         mCanvas.height = height;
-        gl.viewport(0, 0, width, height);
         mWidth = width;
         mHeight = height;
       }
+
+      // Bind to current fbo, using the texture from last
+      gl.bindTexture(gl.TEXTURE_2D, textures[frame % 2]);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[frame % 2]);
+      // gl.bindTexture(gl.TEXTURE_2D, textures[1]);
+      gl.viewport(0, 0, width, height);
 
       gl.clearColor(0, 0, 0, 1);
       gl.clear(COLOR_BUFFER_BIT);
@@ -514,6 +521,7 @@ async function run() {
       const program = programs[0];
       gl.useProgram(program);
 
+      gl.uniform1i(gl.getUniformLocation(program, "u_frame"), frame % 2);
       gl.uniform1f(gl.getUniformLocation(program, "u_time"), now);
       gl.uniform2f(
         gl.getUniformLocation(program, "u_resolution"),
@@ -548,26 +556,31 @@ async function run() {
         0.2 * Math.pow(pendulum.mass2, 0.333)
       );
 
-      // FFT
-      synth.analyser.getByteFrequencyData(fft);
+      // Draw
+      gl.drawArrays(TRIANGLE_STRIP, 0, 4);
 
-      // upload the audio data to the texture
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.texImage2D(
-        gl.TEXTURE_2D,
-        0, // level
-        gl.LUMINANCE, // internal format
-        synth.analyser.frequencyBinCount, // width
-        1, // height
-        0, // border
-        gl.LUMINANCE, // format
-        gl.UNSIGNED_BYTE, // type
-        fft
+      // Effects shader, drawing the scene from texture
+
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+      gl.viewport(0, 0, width, height);
+      gl.clearColor(0, 0, 0, 1);
+      gl.clear(COLOR_BUFFER_BIT);
+
+      const effectsProgram = programs[1];
+      gl.useProgram(effectsProgram);
+
+      gl.uniform2f(
+        gl.getUniformLocation(effectsProgram, "u_resolution"),
+        width,
+        height
       );
+      gl.uniform1i(gl.getUniformLocation(effectsProgram, "u_texture0"), 0);
+      gl.uniform1i(gl.getUniformLocation(effectsProgram, "u_texture1"), 1);
 
-      // bind the texture to texture unit 0
       gl.activeTexture(gl.TEXTURE0);
-      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+      gl.activeTexture(gl.TEXTURE1);
+      gl.bindTexture(gl.TEXTURE_2D, textures[1]);
 
       // Draw
       gl.drawArrays(TRIANGLE_STRIP, 0, 4);
@@ -577,6 +590,22 @@ async function run() {
       console.log(err);
       alert("Error: " + err.message);
     }
+
+    frame += 1;
+  }
+
+  function createAndSetupTexture(gl) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Set up texture so we can render any size image and so we are
+    // working with pixels.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    return texture;
   }
 
   (function setup() {
@@ -595,6 +624,8 @@ async function run() {
 
       const program = gl.createProgram();
       programs[0] = program;
+      const effectsProgram = gl.createProgram();
+      programs[1] = effectsProgram;
 
       let shader = gl.createShader(VERTEX_SHADER);
       gl.shaderSource(shader, vs);
@@ -617,6 +648,27 @@ async function run() {
         alert("Link program: " + gl.getProgramInfoLog(program));
       }
 
+      shader = gl.createShader(VERTEX_SHADER);
+      gl.shaderSource(shader, vs);
+      gl.compileShader(shader);
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Vertex shader: " + gl.getShaderInfoLog(shader));
+      }
+      gl.attachShader(effectsProgram, shader);
+
+      shader = gl.createShader(FRAGMENT_SHADER);
+      gl.shaderSource(shader, fs2);
+      gl.compileShader(shader);
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Fragment shader: " + gl.getShaderInfoLog(shader));
+      }
+      gl.attachShader(effectsProgram, shader);
+
+      gl.linkProgram(effectsProgram);
+      if (!gl.getProgramParameter(effectsProgram, gl.LINK_STATUS)) {
+        alert("Link program: " + gl.getProgramInfoLog(effectsProgram));
+      }
+
       const vertices = [1, 1, 1, -1, -1, 1, -1, -1];
 
       gl.bindBuffer(ARRAY_BUFFER, gl.createBuffer());
@@ -626,13 +678,37 @@ async function run() {
       gl.vertexAttribPointer(0, 2, FLOAT, false, 0, 0);
 
       synth = initializeSynth();
-      fft = new Uint8Array(synth.analyser.frequencyBinCount);
 
-      texture = gl.createTexture();
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      for (var ii = 0; ii < 2; ++ii) {
+        const texture = createAndSetupTexture(gl);
+        textures.push(texture);
+
+        gl.texImage2D(
+          gl.TEXTURE_2D,
+          0,
+          gl.RGBA,
+          window.innerWidth,
+          window.innerHeight,
+          0,
+          gl.RGBA,
+          gl.UNSIGNED_BYTE,
+          null
+        );
+
+        // Create a framebuffer
+        const fbo = gl.createFramebuffer();
+        framebuffers.push(fbo);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+        // Attach a texture to it.
+        gl.framebufferTexture2D(
+          gl.FRAMEBUFFER,
+          gl.COLOR_ATTACHMENT0,
+          gl.TEXTURE_2D,
+          texture,
+          0
+        );
+      }
 
       gui = new dat.GUI();
       gui.add(pendulum, "angle1", 0, 10, 0.01).listen();
--- fragment.glsl
+++ fragment.glsl
@@ -1,4 +1,5 @@
 precision highp float;
+uniform int u_frame;
 uniform float u_time;
 uniform vec2 u_resolution;
 uniform vec4 u_p;
